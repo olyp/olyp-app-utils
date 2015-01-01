0f5f8360f644476a311cc657c7a1a2a981bb2880
August Lilleaas <august@augustl.com>
2015-01-01 21:50:47 +0100

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject olyp-app-utils "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.stuartsierra/component "0.2.2"]
+                 [http-kit "2.1.17"]
+                 [cheshire "5.3.1"]
+                 [org.clojure/tools.logging "0.3.1"]])
---
+++ src/olyp_app_utils/components/olyp_central_api_client_ctx.clj
@@ -0,0 +1,16 @@
+(ns olyp-app-utils.components.olyp-central-api-client-ctx
+  (:require [com.stuartsierra.component :as component])
+  (:import [org.httpkit.client HttpClient]))
+
+(defrecord OlypCentralApiClientCtx [url]
+  component/Lifecycle
+
+  (start [component]
+    (assoc component
+      :httpkit-client (HttpClient.)))
+  (stop [component]
+    (.stop (:httpkit-client component))
+    (dissoc component :httpkit-client)))
+
+(defn create-ctx [{:keys [url]}]
+  (OlypCentralApiClientCtx. url))
